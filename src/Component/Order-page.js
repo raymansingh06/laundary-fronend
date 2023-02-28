a74e01b89bc32e61234ea98ef005f2da5cab0bdb
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-02-27 21:14:54 -0800

    Added new files

--- src/Component/Order-page.js
+++ src/Component/Order-page.js
@@ -8,6 +8,7 @@ const OrderPage = ()=>{
     const[orderData,setOrderData]=useState([]);
     const[get,setget] = useState(true);
     const[oredrhistory,    setorderhistory] = useState(true);
+    const[query,setQuery] =useState("")
 
     useEffect(()=>{
         if(Authou){
@@ -41,9 +42,24 @@ const OrderPage = ()=>{
         }
     }
     return(
-        <div>
+        <>
+        <div className="class">
+    <p className="orderv">Order {orderData.length}</p>
+ <div className="class">
+    <Link to="/create-order"><button className="create">Create</button></Link>
+    <img className="mangnifine" src="/images/search.png" alt=""/>
+    <input type="search1" onChange={event=>setQuery(event.target.value)} className="search"/>
+ </div>
+ <table className="order_table" style={{border:"none"}}>
+    <tr>
+        <th style={{width:"90px"}}>Order Id</th>
+    </tr>
+    <th style={{width:"150px"}}>Order Date and Time</th>
+
+ </table>
 
         </div>
+        </>
     )
 }
 export default OrderPage;
