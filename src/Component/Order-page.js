6a5db2b94798fc12d4d89e357e96392622389d39
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-02-27 20:00:05 -0800

    added orderpage

--- src/Component/Order-page.js
+++ src/Component/Order-page.js
@@ -1,6 +1,49 @@
+
 import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 const OrderPage = ()=>{
-    const[summary,setSummary] = use
+    const[summary,setSummary] = useState(false);
+    const[orderData,setOrderData]=useState([]);
+    const[get,setget] = useState(true);
+    const[oredrhistory,    setorderhistory] = useState(true);
+
+    useEffect(()=>{
+        if(Authou){
+            axious({
+                mathod:'GET',
+                url:"createoredrurl",
+                headers:{
+                    
+                }
+            }).then((datas)=>{
+                setorderhistory(true);
+                //console.log(currorder);
+            setOrderData(datas.data);
+            })
+        }else{
+            Navigate("/Signin")
+        }
+    },[])
+
+    const handleView=(data)=>{
+        setViewdata(data);
+    }
+
+    const selecthandler = (e)=>{
+        if(e.target.value === "Store location"){
+            setget(!get)
+        }else{
+            const newdata = orderData.filter((item)=>{
+                if("")
+            })
+        }
+    }
+    return(
+        <div>
+
+        </div>
+    )
 }
+export default OrderPage;
